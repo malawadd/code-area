bc4bcb94735db179cc6d2b5d4c875c11711fa70e
malawad <50057882+malawadd@users.noreply.github.com>
2024-01-30 03:23:40 +0300

    ✅ updated scenes ✅

---
+++ src/game/scenes/menu/interface/content/about/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Wrapper } from "./styles";
+
+export const About: React.FC = () => (
+  <Wrapper>
+    Welcome to our experimental game powered by phi assets!
+    <br />
+    <br />
+    Don't forget to enhance the abilities of your avatar and pets, utilizing the
+    unique features of phi assets.
+    <br />
+    <br />
+    Dive in and experience the next generation of gaming!
+  </Wrapper>
+);
